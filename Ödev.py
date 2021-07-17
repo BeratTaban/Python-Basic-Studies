5cc1491180bf05eff498e4626069906e79210807
Berat <85937486+BeratTaban@users.noreply.github.com>
2021-07-17 16:07:51 +0300

    Add files via upload

---
+++ Ödev.py
@@ -0,0 +1,11 @@
+print("Kayıt Ol")
+Ad = input("Oyuncu Adı:")
+Soyad = input("Oyuncu Soyadı:")
+NickName = input("Oyuncu NickName:")
+Mail = input("Oyuncu Mail:")
+
+içerik = [Ad,Soyad,NickName,Mail]
+print("Lütfen Bekleyiniz...\nİşlem Tamamlanıyor...")
+
+print("Oyuncu Adı:{}\nOyuncu Soyadı:{}\nOyuncu NickName:{}\nOyuncu Mail:{}\n".format(içerik[0],içerik[1],içerik[2],içerik[3]))
+print("İşlem Tamamlandı...")
